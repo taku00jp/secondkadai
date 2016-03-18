db3e248408fc11bc0752ab969062efc98af545ec
taku00jp <saitou@saitou-no-imac.local>
2016-03-19 03:58:31 +0900

    修正

---
+++ SecondViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SecondViewController.swift
+//  secondkadai
+//
+//  Created by 斉藤 on 2016/03/19.
+//  Copyright © 2016年 taku.saito. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+   var sendText:String = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+  
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- secondkadai/ViewController.swift
+++ secondkadai/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if segue.identifier == "SecondView" {
+            
+            let secondViewController:SecondViewController = segue.destinationViewController as! SecondViewController
+            
+            
+          secondViewController.sendText = self.textField.text!
+        }
 
 }
 
+}
