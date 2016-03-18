9d9800e451ddf08fdebb3be8d0c18f8238fa0ae2
taku00jp <saitou@saitou-no-imac.local>
2016-03-19 02:53:46 +0900

    Initial Commit

---
+++ secondkadai/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  secondkadai
+//
+//  Created by 斉藤 on 2016/03/19.
+//  Copyright © 2016年 taku.saito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
